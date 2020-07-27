960c8414169722537311162ae9afb4b4609e2e78
li_jia <1229844289@qq.com>
2020-07-27 14:24:39 +0800

    v0.1.0

--- src/SmartyConfig/Common/SmartyConfig.php
+++ src/SmartyConfig/Common/SmartyConfig.php
@@ -6,9 +6,9 @@ class SmartyConfig
     public static function configDir()
     {
         $dirArray = array(
-            APP_ROOT.'vendor/qixinyun/taiyuan-portal-config/src/SmartyConfig/Ty',
-            APP_ROOT.'vendor/qixinyun/taiyuan-portal-config/src/SmartyConfig/Common',
-            APP_ROOT.'vendor/qixinyun/taiyuan-portal-config/src/SmartyConfig'
+            APP_ROOT.'vendor/qixinyun/taiyuan-portal-local-config/src/SmartyConfig/Ty',
+            APP_ROOT.'vendor/qixinyun/taiyuan-portal-local-config/src/SmartyConfig/Common',
+            APP_ROOT.'vendor/qixinyun/taiyuan-portal-local-config/src/SmartyConfig'
         );
 
         return $dirArray;
